773ead9956b28d5797192271d84141f83785c12e
dbaa omar <dbaaomar18@gmail.com>
2023-11-02 16:49:57 +0100

    data aggregation and analysis for user profiles dashboard added

---
+++ dashboardQuery.py
@@ -0,0 +1,139 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pymongo
+import pandas as pd
+import plotly.express as px
+
+# Initialize MongoDB Connection
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017")
+db = mongo_client["user_profiles"]
+collection = db["user_collection"]
+
+# Initialize Dash App
+app = dash.Dash(__name__)
+
+# Define Layout
+app.layout = html.Div([
+    html.H1("User Profiles Analysis Dashboard", style={'textAlign': 'center'}),
+    
+    html.Button("Aggregate Data", id="aggregate-button", style={'margin': '20px'}),
+    
+    dcc.Graph(id='user-count-bar', config={'displayModeBar': False}),
+    dcc.Graph(id='average-age-bar', config={'displayModeBar': False}),
+    dcc.Graph(id='average-age-by-country-map', config={'displayModeBar': False}),  # Choropleth map
+    dcc.Graph(id='email-domains-pie', config={'displayModeBar': False}),
+])
+
+# Callbacks to update graphs
+@app.callback(
+    [Output('user-count-bar', 'figure'),
+     Output('average-age-bar', 'figure'),
+     Output('average-age-by-country-map', 'figure'),
+     Output('email-domains-pie', 'figure')],  # Most Common Email Domains
+    [Input('aggregate-button', 'n_clicks')]
+)
+def update_graphs(n_clicks):
+    # MongoDB Aggregation Pipeline
+    pipeline = [
+        {
+            "$project": {
+                "_id": 0,
+                "nationality": 1,
+                "age": 1,
+                "country": 1
+            }
+        },
+        {
+            "$group": {
+                "_id": "$nationality",
+                "user_count": {"$sum": 1},
+                "avg_age": {"$avg": "$age"}
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "nationality": "$_id",
+                "user_count": 1,
+                "avg_age": 1
+            }
+        }
+    ]
+
+    data = list(collection.aggregate(pipeline))
+
+    df = pd.DataFrame(data)
+    
+    # Aggregation: Number of Users by Nationality
+    user_count_fig = px.bar(df, x='nationality', y='user_count', labels={'user_count': 'User Count'})
+    user_count_fig.update_layout(
+        title_text='Number of Users by Nationality',
+        xaxis_title="Nationality",
+        yaxis_title="User Count",
+        showlegend=True,
+        legend_title_text="Legend Title",
+    )
+    
+    # Aggregation: Average Age of Users by Nationality
+    avg_age_bar_fig = px.bar(df, x='nationality', y='avg_age', labels={'avg_age': 'Average Age'})
+    avg_age_bar_fig.update_layout(
+        title_text='Average Age of Users by Nationality',
+        xaxis_title="Nationality",
+        yaxis_title="Average Age",
+        showlegend=True,
+        legend_title_text="Legend Title",
+    )
+
+    # Aggregation: Average Age of Users by Country
+    pipeline_country = [
+        {
+            "$project": {
+                "_id": 0,
+                "country": 1,
+                "age": 1
+            }
+        },
+        {
+            "$group": {
+                "_id": "$country",
+                "avg_age": {"$avg": "$age"}
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "country": "$_id",
+                "avg_age": 1
+            }
+        }
+    ]
+
+    data_country = list(collection.aggregate(pipeline_country))
+    df_country = pd.DataFrame(data_country)
+
+    avg_age_by_country_map = px.choropleth(df_country, 
+                                           locations="country", 
+                                           locationmode="country names", 
+                                           color="avg_age", 
+                                           hover_name="country",
+                                           color_continuous_scale=px.colors.sequential.Plasma,
+                                           title="Average Age of Users by Country")
+    
+    avg_age_by_country_map.update_geos(
+        showcoastlines=True, coastlinecolor="Black",
+        showland=True, landcolor="white"
+    )
+
+    # For the email domains, create an empty pie chart (null graph)
+    email_domains_fig = px.pie(names=["No Data"], values=[1])
+    email_domains_fig.update_layout(
+        title_text='Most Common Email Domains',
+        showlegend=True,
+        legend_title_text="Legend Title",
+    )
+
+    return user_count_fig, avg_age_bar_fig, avg_age_by_country_map, email_domains_fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
